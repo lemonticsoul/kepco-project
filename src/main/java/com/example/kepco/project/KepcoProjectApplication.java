1e024bbf7898fe4993eb8e5e5e7d06af2241df80
lemonticsoul <127959482+lemonticsoul@users.noreply.github.com>
2025-01-27 16:32:52 +0900

    Initial commit

---
+++ src/main/java/com/example/kepco/project/KepcoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kepco.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KepcoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KepcoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kepco/project/KepcoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kepco.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KepcoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
